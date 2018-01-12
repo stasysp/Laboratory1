647e1a6c5dc32fb4efa5c6c49d7a3cf4b3639cf5
stasysp <stasysp.96@gmail.com>
2018-01-12 10:05:55 +0300

    Update execute.py
    add execute function

--- execute.py
+++ execute.py
@@ -1,3 +1,5 @@
+import operator
+import re
 
 def parse_token(token):
     # ???
@@ -6,5 +8,38 @@ def parse_token(token):
 OPERATOR_TO_LAMBDA = {
 }
 
-def execute(line):
-    return 0.0
+def execute(expr):
+    OPERATORS = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
+    for op in OPERATORS.keys():
+        expr = expr.replace(op, ' ' + op + ' ')
+    expr = re.sub(' +',' ', expr)
+    
+    #check correctness
+    stack_size = 0
+    for token in expr.split(' '):
+        if token.replace('.', '').isnumeric():
+            stack_size += 1
+        elif token in '+-*/':
+            stack_size -= 1
+        else:
+            raise ValueError('Incorrect Reverse Polish Notation')
+        if stack_size < 0:
+            raise ValueError('Incorrect Reverse Polish Notation')
+    if stack_size != 0:
+        raise ValueError('Incorrect Reverse Polish Notation')
+    
+    #calculate
+    stack = [0]
+    for token in expr.split(" "):
+        if token in OPERATORS:
+            op2, op1 = stack.pop(), stack.pop()
+            if '.' in op1 or '.' in op2:
+                op1, op2 = float(op1), float(op2)
+                stack.append(str(OPERATORS[token](op1,op2)))
+            else:                
+                op1, op2 = int(op1), int(op2)
+                stack.append(str(int(OPERATORS[token](op1,op2))))
+        elif token:
+            stack.append(token)
+    res = stack.pop()
+    return float(res) if '.' in res else int(res)
