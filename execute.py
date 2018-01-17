b32650ce1d8d6f6b057b66f9b8dc1368d8696d54
stasysp <stasysp.96@gmail.com>
2018-01-17 10:18:49 +0300

    Update execute.py

--- execute.py
+++ execute.py
@@ -7,42 +7,31 @@ def parse_token(token):
 
 OPERATOR_TO_LAMBDA = {
 }
-
+ 
 def execute(expr):
     OPERATORS = {'+': operator.add, '-': operator.sub, '*': operator.mul, '/': operator.truediv}
     for op in OPERATORS.keys():
         expr = expr.replace(op, ' ' + op + ' ')
     expr = re.sub(' +',' ', expr)
     expr = expr.strip(' ')
-    
-    #check correctness
-    stack_size = 0
     index = 0
-    for token in expr.split(' '):
-        if token.replace('.', '').isnumeric():
-            stack_size += 1
-        elif token in '+-*/':
-            stack_size -= 1
-        else:
-            raise ValueError('Incorrect Reverse Polish Notation \n' + expr + '\n' + ' ' * (2 * index) + '^')
-        if stack_size < 1 and token != expr[-1]:
-            raise ValueError('Incorrect Reverse Polish Notation \n' + expr + '\n' + ' ' * (2 * index) + '^')
-        index += 1
-    if stack_size != 1:
-        raise ValueError('Incorrect Reverse Polish Notation \n' + expr + '\n' + ' ' * (2 * index - 1) + '^')
     
     #calculate
     stack = [0]
     for token in expr.split(" "):
-        if token in OPERATORS:
-            op2, op1 = stack.pop(), stack.pop()
-            if '.' in op1 or '.' in op2:
-                op1, op2 = float(op1), float(op2)
-                stack.append(str(OPERATORS[token](op1,op2)))
-            else:                
-                op1, op2 = int(op1), int(op2)
-                stack.append(str(int(OPERATORS[token](op1,op2))))
-        elif token:
-            stack.append(token)
+        try:
+            if token in OPERATORS:
+                op2, op1 = stack.pop(), stack.pop()
+                if '.' in op1 or '.' in op2:
+                    op1, op2 = float(op1), float(op2)
+                    stack.append(str(OPERATORS[token](op1,op2)))
+                else:                
+                    op1, op2 = int(op1), int(op2)
+                    stack.append(str(int(OPERATORS[token](op1,op2))))
+            elif token:
+                stack.append(token)
+        except:
+            raise ValueError('Incorrect Reverse Polish Notation \n' + expr + '\n' + ' ' * (2 * index) + '^')
+        index += 1
     res = stack.pop()
     return float(res) if '.' in res else int(res)
